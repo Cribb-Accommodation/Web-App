e07add406dab4a56d55d087c338ab2b639a9f330
Chibuike Ewenike <sopewenike@yahoo.com>
2023-11-08 09:49:40 -0800

    first draft landing page

--- src/components/LandingPage/services/index.tsx
+++ src/components/LandingPage/services/index.tsx
@@ -4,18 +4,18 @@ import { servicesList, servicesListT } from "./data";
 function Services() {
   return (
     <section>
-      <div className=" bg-bg_primary px-[1rem] pb-[7.44rem] lg:px-[3.5rem] l1440:px-[7.5rem]">
+      <div className=" bg-bg_primary px-[1rem] pb-[7.44rem] pt-[3.81rem] lg:pt-[7.91rem] llg:pb-[15.5rem] lg:px-[3.5rem] l1440:px-[7.5rem]">
         <h2 className="mb-[5.41rem] text-primary_text text-[2rem] font-bold font-playfair">
           What we offer
         </h2>
         <div className="lg:grid gap-[4.5rem] grid-cols-2">
           {servicesList.map((item: servicesListT, key: number) => (
-            <div key={key} className="">
+            <div key={key} className="max-lg:mb-[3.81rem]">
               {item.svg}
-              <h3 className="text-primary_text text-[1.5rem] font-semibold font-playfair mt-[1rem]">
+              <h3 className="text-primary_text text-[1.25rem] md:text-[1.5rem] font-semibold font-playfair mt-[1rem]">
                 {item.heading}
               </h3>
-              <h3 className="text-subText font-poppins text-base font-normal mt-[.5rem]//">
+              <h3 className="text-subText font-poppins text-[0.875rem] lg:text-base font-normal mt-[.5rem]">
                 {item.details}
               </h3>
             </div>
